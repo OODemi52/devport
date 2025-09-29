ff12b306760fd98fdc031dc8db8255a7fe61a00b
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2025-09-29 12:59:47 -0400

    styling fix

--- components/commands/Education.tsx
+++ components/commands/Education.tsx
@@ -4,34 +4,45 @@ import React from "react";
 
 const Education: React.FC = () => {
   return (
-    <>
-      <p>
-        <strong>Master of Science (M.Sc)</strong>
-        <em>
+    <ul>
+      <li>
+        <p>
+          <strong>Master of Science (M.Sc)</strong>
+          <em>
+            <br />
+            Information Systems and Technology
+            <br />
+            with Graduate Certificate in Artificial Intelligence
+          </em>
           <br />
-          Information Systems and Technology
-          <br />
-          with Graduate Certificate in Artificial Intelligence
-        </em>
-        <br />
-        <a className="info" href="https://www.gwu.edu" target="_blank">
-          The George Washington University
-        </a>{" "}
-        | 2022 - 2024
-      </p>
-      <p>
-        <strong>Bachelor of Science (B.Sc)</strong>
-        <em>
-          <br />
-          Mechanical Engineering
-          <br />
-        </em>
-        <a className="info" href="https://umbc.edu" target="_blank">
-          University of Marylnad - Baltimore County
-        </a>{" "}
-        | 2016 - 2020
-      </p>
-    </>
+          <a
+            className="info underline"
+            href="https://www.gwu.edu"
+            target="_blank"
+          >
+            The George Washington University
+          </a>{" "}
+          | 2022 - 2024
+        </p>
+      </li>
+      <br />
+
+      <li>
+        <p>
+          <strong>Bachelor of Science (B.Sc)</strong>
+          <em>
+            <br />
+            Mechanical Engineering
+            <br />
+          </em>
+          <a className="info underline" href="https://umbc.edu" target="_blank">
+            University of Marylnad - Baltimore County
+          </a>{" "}
+          | 2016 - 2020
+        </p>
+      </li>
+      <br />
+    </ul>
   );
 };
 
